998e64be57552093704d9c0c342164d4cf054ee9
SadKidBGMZ <104976581+SadKidBGMZ@users.noreply.github.com>
2022-12-11 21:44:08 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,13 +1,13 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/SadKidBGMZ/Rocky_Umar75.git /Rocky_Umar75
+  git clone https://github.com/SadKidBGMZ/Rocky_Filter_Bot.git /Rocky_Filter_Bot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone https://github.com/SadKidBGMZ/Rocky_Umar75
+  git clone https://github.com/SadKidBGMZ/Rocky_Filter_Bot
 fi
 ls
-cd SadKidBGMZ/Rocky_Umar75
+cd SadKidBGMZ/Rocky_Filter_Bot
 ls
 $PWD
 pip3 install -U -r requirements.txt

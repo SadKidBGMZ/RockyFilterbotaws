ac85fe296a2a925236ae34053465de8e1966dee3
SadKidBGMZ <104976581+SadKidBGMZ@users.noreply.github.com>
2022-12-15 21:15:15 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,15 +1,1 @@
-if [ -z $UPSTREAM_REPO ]
-then
-  echo "Cloning main Repository"
-  git clone https://github.com/SadKidBGMZ/RockyFilterBotaws.git /RockyFilterBotaws
-else
-  echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone https://github.com/SadKidBGMZ/RockyFilterBotaws
-fi
-ls
-cd RockyFilterBotaws
-ls
-$PWD
-pip3 install -U -r requirements.txt
-echo "Starting Bot...."
 python3 bot.py
